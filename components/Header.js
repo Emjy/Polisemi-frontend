52f654378dd47979c55adb2c5bfd09823b266d12
Dang-Minh TRAN <dang-minh.tran@hotmail.com>
2024-05-14 14:55:38 +0200

    homepage design done

---
+++ components/Header.js
@@ -0,0 +1,28 @@
+import styles from '../styles/Header.module.css';
+import Link from 'next/link'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXTwitter, faInstagram} from '@fortawesome/free-brands-svg-icons'
+
+function Header() {
+
+    return (
+        <div>
+            <header className={styles.banner}>
+
+            <Link href='/'><div className={styles.logobanner}>Polisemi</div></Link>
+
+                <div className={styles.bannertext}>
+                    <Link href='/calendar'><div className={styles.link}>Calendrier</div></Link>
+                    <Link href='/contact'><div className={styles.link}>Contact</div></Link>
+                    <Link href='https://www.instagram.com/polisemi.paris14/' target="_blank"><FontAwesomeIcon icon={faInstagram} className={styles.picto} size="2xl"/></Link>
+                    <Link href='https://twitter.com/polisemi_75014?lang=fr' target="_blank"><FontAwesomeIcon icon={faXTwitter} className={styles.picto} size="2xl"/></Link>
+            
+                </div>
+            </header>
+
+        </div>
+    )
+}
+
+export default Header
--- components/Home.js
+++ components/Home.js
@@ -1,13 +1,97 @@
 import styles from '../styles/Home.module.css';
+import Header from './Header';
 
 function Home() {
   return (
     <div>
+
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <Header />
+        {/* <img src='musee_bourdelle.webp'/> */}
+        <div className={styles.text}>
+          <h1 className={styles.title}>The Center for the Less Good Idea</h1>
+          <div className={styles.description}>William Kentridge, Bronwyn Lace et plus de trente artistes en résidence</div>
+          <div>
+            <div className={styles.date}>Du mardi 14 mai 2024 au lundi 20 mai 2024</div>
+            <div className={styles.hour}>19h00 — 22h00</div>
+            <div className={styles.place}>Fondation Cartier</div>
+          </div>
+        </div>
       </main>
+
+      <section className={styles.events}>
+
+        <div className={styles.card}>
+          <img src='musee_bourdelle.webp' />
+          <h1 className={styles.title}>The Center for the Less Good Idea</h1>
+          <div className={styles.description}>William Kentridge, Bronwyn Lace et plus de trente artistes en résidence</div>
+          <div>
+            <div className={styles.date}>Du mardi 14 mai 2024 au lundi 20 mai 2024</div>
+            <div className={styles.hour}>19h00 — 22h00</div>
+            <div className={styles.place}>Fondation Cartier</div>
+          </div>
+        </div>
+
+        <div className={styles.card}>
+          <img src='musee_bourdelle.webp' />
+          <h1 className={styles.title}>The Center for the Less Good Idea</h1>
+          <div className={styles.description}>William Kentridge, Bronwyn Lace et plus de trente artistes en résidence</div>
+          <div>
+            <div className={styles.date}>Du mardi 14 mai 2024 au lundi 20 mai 2024</div>
+            <div className={styles.hour}>19h00 — 22h00</div>
+            <div className={styles.place}>Fondation Cartier</div>
+          </div>
+        </div>
+
+        <div className={styles.card}>
+          <img src='musee_bourdelle.webp' />
+          <h1 className={styles.title}>The Center for the Less Good Idea</h1>
+          <div className={styles.description}>William Kentridge, Bronwyn Lace et plus de trente artistes en résidence</div>
+          <div>
+            <div className={styles.date}>Du mardi 14 mai 2024 au lundi 20 mai 2024</div>
+            <div className={styles.hour}>19h00 — 22h00</div>
+            <div className={styles.place}>Fondation Cartier</div>
+          </div>
+        </div>
+
+        <div className={styles.card}>
+          <img src='musee_bourdelle.webp' />
+          <h1 className={styles.title}>The Center for the Less Good Idea</h1>
+          <div className={styles.description}>William Kentridge, Bronwyn Lace et plus de trente artistes en résidence</div>
+          <div>
+            <div className={styles.date}>Du mardi 14 mai 2024 au lundi 20 mai 2024</div>
+            <div className={styles.hour}>19h00 — 22h00</div>
+            <div className={styles.place}>Fondation Cartier</div>
+          </div>
+        </div>
+
+        <div className={styles.card}>
+          <img src='musee_bourdelle.webp' />
+          <h1 className={styles.title}>The Center for the Less Good Idea</h1>
+          <div className={styles.description}>William Kentridge, Bronwyn Lace et plus de trente artistes en résidence</div>
+          <div>
+            <div className={styles.date}>Du mardi 14 mai 2024 au lundi 20 mai 2024</div>
+            <div className={styles.hour}>19h00 — 22h00</div>
+            <div className={styles.place}>Fondation Cartier</div>
+          </div>
+        </div>
+
+        <div className={styles.card}>
+          <img src='musee_bourdelle.webp' />
+          <h1 className={styles.title}>The Center for the Less Good Idea</h1>
+          <div className={styles.description}>William Kentridge, Bronwyn Lace et plus de trente artistes en résidence</div>
+          <div>
+            <div className={styles.date}>Du mardi 14 mai 2024 au lundi 20 mai 2024</div>
+            <div className={styles.hour}>19h00 — 22h00</div>
+            <div className={styles.place}>Fondation Cartier</div>
+          </div>
+        </div>
+
+
+      </section>
+
+
+
     </div>
   );
 }
